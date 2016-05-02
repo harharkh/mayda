c36e143d408c9cc4ff933bfaa919e84a62e61e3f
Jeremy Mason <jkylemason@gmail.com>
2016-05-02 16:10:22 +0300

    Added benches for other widths.

--- benches/binary.rs
+++ benches/binary.rs
@@ -59,7 +59,10 @@ encode_bench!{
   (u32, 1, 32, en_u32_1_32),
   (u32, 3, 65535, en_u32_3_65535),
   (u32, 3, 65536, en_u32_3_65536),
-  (u32, std::u32::MAX, 65536, en_u32_MAX_65536)
+  (u8, std::u8::MAX, 65536, en_u8_MAX_65536),
+  (u16, std::u16::MAX, 65536, en_u16_MAX_65536),
+  (u32, std::u32::MAX, 65536, en_u32_MAX_65536),
+  (u64, std::u64::MAX, 65536, en_u64_MAX_65536)
 }
 
 decode_bench!{
@@ -67,7 +70,10 @@ decode_bench!{
   (u32, 2, 32, de_u32_2_32),
   (u32, 4, 65535, de_u32_4_65535),
   (u32, 4, 65536, de_u32_4_65536),
-  (u32, std::u32::MAX, 65536, de_u32_MAX_65536)
+  (u8, std::u8::MAX, 65536, de_u8_MAX_65536),
+  (u16, std::u16::MAX, 65536, de_u16_MAX_65536),
+  (u32, std::u32::MAX, 65536, de_u32_MAX_65536),
+  (u64, std::u64::MAX, 65536, de_u64_MAX_65536)
 }
 
 vector_bench!{
